f0d80bd1b721ede74b6ca4267bb3fa72e8244713
Binjuhor <binjuhor@gmail.com>
2018-09-10 22:42:11 +0700

    v1.0.0 + Try with dynamic class.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,13 +3,15 @@ import React, { Component } from 'react'
 class Counter  extends Component {
     state = {
         count: 0,
-        imageUrl: "https://picsum.photos/200"
+        tags: ['tag1', 'tag2', 'tag3', 'tag4']
     }
 
     render(){
+        let classes = "adge badge-"
+        classes += ( this.state.count === 0 ) ? "warning" : "primary"
         return (
             <div>
-                <span className="badge badge-primary m-2">{ this.formatCount() }</span>
+                <span className="{ classes }">{ this.formatCount() }</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         )
