e0e3509c515c8c571d76cf6edb2a0155bc0d6591
Binjuhor <binjuhor@gmail.com>
2018-09-22 00:21:29 +0700

    v1.0.0 - Learn ReactJs + I know how my Class does not display. + Update state and view. + OnClick function. + Arrow function.

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,28 +3,46 @@ import React, { Component } from 'react'
 class Counter  extends Component {
     state = {
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3', 'tag4']
+        tags: ['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6']
     }
 
-    render(){
+
+    // constructor(){
+    //     super()
+    //     this.handleIncrement = this.handleIncrement.bind( this )
+    // }
+
+    // handleIncrement() {
+    //     console.log('What the fuck?', this )
+    // }
+
+    handleIncrement = () =>{
+        this.setState( { count: this.state.count+1 } )
+    }
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>
+
+        return <ul>{this.state.tags.map(tag => <li key={tag}> {tag} </li>)}</ul>
+    }
+
+    render() {
         return (
             <div>
-                <span className="{this.getBadgeMethod()}">{ this.formatCount() }</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    { this.state.tags.map( tag => <li key=" {tag} "> { tag } </li>) }
-                </ul>
+                <span className={ this.getBadgeMethod() }>{ this.formatCount() }</span>
+                <button onClick={ this.handleIncrement } className="btn btn-secondary btn-sm">Increment</button>
+                { this.renderTags() }
             </div>
         )
     }
 
-    getBadgeMethod(){
+    getBadgeMethod() {
         let classes = "badge m-2 badge-"
         classes += (this.state.count === 0) ? "warning" : "primary"
         return classes
     }
 
-    formatCount(){
+    formatCount() {
         const { count } = this.state
         return count === 0 ? "Zero" : count
     }
